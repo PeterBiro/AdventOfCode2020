fb7f9bc5383c1c2e57e29cd425a8db1dbaca0378
PeterBiro <biro.peter.hu@gmail.com>
2020-12-01 21:45:25 +0100

    day1-2

--- AoC_2020.py
+++ AoC_2020.py
@@ -3,28 +3,43 @@ import sys
 
 def read_numbers_from_file(puzzle):
     filename = "input_{}.txt".format(puzzle)
-
     with open(filename, "r") as f:
         data_list = f.readlines()
-
     return list(map(int, data_list))
 
 
-def day_1_1():
+def find_additive_pair_in_list(goal, sequence):
+    set_of_nums = set(sequence)
+    for first_num in sequence:
+        if goal - first_num in set_of_nums:
+            print("Hmm, it seems valuable: {} + {} = {}".format(first_num, goal - first_num, goal))
+            return first_num*(goal - first_num)
+    print("not found :(")
+    return 0
 
+
+def day_1_1():
     numbers = read_numbers_from_file("day1-1")
-    set_of_nums = set(numbers)
-    print("length of list: {}".format(len(numbers)))
-    for first_num in numbers:
-        if 2020 - first_num in set_of_nums:
-            print("HEUREKA: {}".format(first_num*(2020 - first_num)))
-            return
-    print("sadly not found")
+    result = find_additive_pair_in_list(2020, numbers)
+    print("HEUREKA: {}".format(result))
+
+
+def day_1_2():
+    numbers = read_numbers_from_file("day1-1")
+    for i in range(len(numbers)-1):
+        num = numbers[i]
+        print("Checking {} ...".format(num), end="")
+        result = find_additive_pair_in_list(2020-num, numbers[i+1:])
+        if result:
+            print("HEUREKA: {}".format(result*num))
+            break
 
 
 def main(args):
     if args[0] == "day1-1":
         day_1_1()
+    elif args[0] == "day1-2":
+        day_1_2()
     else:
         print("Unknown argument: {}, and the full list: {}".format(args[0], args))
 
