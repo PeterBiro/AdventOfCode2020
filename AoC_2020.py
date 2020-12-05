0b99513e80b219ca496cdbf9aca9e1d07b563d14
PeterBiro <biro.peter.hu@gmail.com>
2020-12-05 15:23:33 +0100

    add task and puzzle enum classes and do some refactor

--- AoC_2020.py
+++ AoC_2020.py
@@ -1,6 +1,20 @@
 import sys
 import re
+from enum import Enum
 from operator import add  # day3
+
+
+class Task(Enum):
+    FIRST = "first"
+    SECOND = "second"
+
+
+class Puzzle(Enum):
+    DAY_1 = "day1"
+    DAY_2 = "day2"
+    DAY_3 = "day3"
+    DAY_4 = "day4"
+    DAY_5 = "day5"
 
 
 def read_numbers_from_file(puzzle):
@@ -9,161 +23,125 @@ def read_numbers_from_file(puzzle):
 
 
 def read_strings_from_file(puzzle, keepends=False):
-    filename = "input_{}.txt".format(puzzle, keepends=keepends)
+    filename = "input_{}.txt".format(puzzle.value)
     with open(filename, "r") as f:
-        data_list = f.read().splitlines()
+        data_list = f.read().splitlines(keepends=keepends)
     return data_list
 
 
-def find_additive_pair_in_list(goal, sequence):
-    set_of_nums = set(sequence)
-    for first_num in sequence:
-        if goal - first_num in set_of_nums:
-            print("Hmm, it seems valuable: {} + {} = {}".format(first_num, goal - first_num, goal))
-            return first_num*(goal - first_num)
-    print("not found :(")
-    return 0
+def day_1(puzzle, task):
 
+    def find_additive_pair_in_list(goal, sequence):
+        set_of_nums = set(sequence)
+        for first_num in sequence:
+            if goal - first_num in set_of_nums:
+                return first_num * (goal - first_num)
+        return 0
 
-def parse_password_policies(raw_lines):
-    """
-    day 2: from list of strings to list of dicts
-    e.g.: '10-17 r: rrrrrrrrrhrrrrrrrr\n' --> {'min': 10, 'max': 17, 'letter': 'r' , 'psw': 'rrrrrrrrrhrrrrrrrr'}
+    numbers = read_numbers_from_file(puzzle)
 
-    :param raw_lines: list of stings
-    :returns: list of dicts {min: <int>, max: <int>, letter: <char> , psw: <strings>}
-    """
+    if task is Task.FIRST:
+        return find_additive_pair_in_list(2020, numbers)
 
-    def make_them_int(x):
-        x["min"] = int(x["min"])
-        x["max"] = int(x["max"])
-        return x
-
-    result = []
-    pattern = r"(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<psw>[a-z]+)"
-    for line in raw_lines:
-        x = re.search(pattern, line)
-        result.append(make_them_int(x.groupdict()))
-
-    return result
-
-
-def read_passports():
-    raw_data = read_strings_from_file("day4-1")
-    passports = []
-    new_passport = []
-    for line in raw_data:
-        if line == "":
-            passports.append(" ".join(new_passport))
-            new_passport = []
-        else:
-            new_passport.append(line)
-    if new_passport:
-        passports.append(" ".join(new_passport))
-    return passports
-
-
-def create_dict_from_string(passport_strings):
-    dict_list = []
-    for line in passport_strings:
-        element = {}
-        for x in line.split():
-            key, value = x.split(":")
-            element[key] = value
-        dict_list.append(element)
-    return dict_list
-
-
-def day_1_1():
-    numbers = read_numbers_from_file("day1-1")
-    result = find_additive_pair_in_list(2020, numbers)
-    print("HEUREKA: {}".format(result))
-
-
-def day_1_2():
-    numbers = read_numbers_from_file("day1-1")
     for i in range(len(numbers)-1):
         num = numbers[i]
-        print("Checking {} ...".format(num), end="")
         result = find_additive_pair_in_list(2020-num, numbers[i+1:])
         if result:
-            print("HEUREKA: {}".format(result*num))
-            break
+            return result*num
 
 
-def day_2_logic(is_valid):
-    raw_lines = read_strings_from_file("day2-1")
+def day_2(puzzle, task):
+
+    def parse_password_policies(raw_lines):
+        """
+        day 2: from list of strings to list of dicts
+        e.g.: '10-17 r: rrrrrrrrrhrrrrrrrr\n' --> {'min': 10, 'max': 17, 'letter': 'r' , 'psw': 'rrrrrrrrrhrrrrrrrr'}
+
+        :param raw_lines: list of stings
+        :returns: list of dicts {min: <int>, max: <int>, letter: <char> , psw: <strings>}
+        """
+
+        def make_them_int(x):
+            x["min"] = int(x["min"])
+            x["max"] = int(x["max"])
+            return x
+
+        result = []
+        pattern = r"(?P<min>\d+)-(?P<max>\d+) (?P<letter>[a-z]): (?P<psw>[a-z]+)"
+        for line in raw_lines:
+            x = re.search(pattern, line)
+            result.append(make_them_int(x.groupdict()))
+
+        return result
+
+    def create_dict_from_string(passport_strings):
+        dict_list = []
+        for line in passport_strings:
+            element = {}
+            for x in line.split():
+                key, value = x.split(":")
+                element[key] = value
+            dict_list.append(element)
+        return dict_list
+
+    def is_valid(policy):
+        if task is Task.FIRST:
+            occurrence = policy["psw"].count(policy["letter"])
+            return policy["min"] <= occurrence <= policy["max"]
+        else:
+            pos_1 = policy["min"] - 1
+            pos_2 = policy["max"] - 1
+            char = policy["letter"]
+            return (char == policy["psw"][pos_1]) != (char == policy["psw"][pos_2])
+
+    raw_lines = read_strings_from_file(puzzle)
     parsed_lines = parse_password_policies(raw_lines)
     valid_psw_counter = 0
     for policy in parsed_lines:
         if is_valid(policy):
             valid_psw_counter += 1
-    print(valid_psw_counter)
+    return valid_psw_counter
 
 
-def day_2_1():
+def day_3(puzzle, task):
 
-    def is_valid_psw(policy):
-        occurrence = policy["psw"].count(policy["letter"])
-        return policy["min"] <= occurrence <= policy["max"]
+    def trees_in_line(tree_map, v_heading):
+        location = [0, 0]
+        row_len = len(tree_map[0])
+        tree_hit_count = 0
+        while location[1] < len(tree_map) - 1:
+            location = list(map(add, location, v_heading))
+            x = location[0] % row_len
+            y = location[1]
+            if tree_map[y][x] == "#":
+                tree_hit_count += 1
+        return tree_hit_count
 
-    day_2_logic(is_valid_psw)
+    tree_map = read_strings_from_file(puzzle)
+    headings = [(3, 1)] if task is Task.FIRST else [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
 
-
-def day_2_2():
-
-    def is_valid_psw(policy):
-        pos_1 = policy["min"]-1
-        pos_2 = policy["max"]-1
-        char = policy["letter"]
-        return (char == policy["psw"][pos_1]) != (char == policy["psw"][pos_2])
-
-    day_2_logic(is_valid_psw)
-
-
-def day_3_logic(tree_map, v_heading):
-    location = [0, 0]
-    row_len = len(tree_map[0])
-    tree_hit_count = 0
-    while location[1] < len(tree_map) - 1:
-        location = list(map(add, location, v_heading))
-        x = location[0] % row_len
-        y = location[1]
-        if tree_map[y][x] == "#":
-            tree_hit_count += 1
-    return tree_hit_count
-
-
-def day_3_1():
-    tree_map = read_strings_from_file("day3-1")
-    hits = day_3_logic(tree_map, (3, 1))
-    print(hits)
-
-
-def day_3_2():
-    tree_map = read_strings_from_file("day3-1")
-    headings = [(1, 1),
-                (3, 1),
-                (5, 1),
-                (7, 1),
-                (1, 2)]
     hits = 1
     for vec in headings:
-        hits *= day_3_logic(tree_map, vec)
-    print(hits)
+        hits *= trees_in_line(tree_map, vec)
+
+    return hits
 
 
-def day_4_logic(is_valid):
-    passport_strings = read_passports()
-    passports = create_dict_from_string(passport_strings)
-    valid_pports = 0
-    for p in passports:
-        if is_valid(p):
-            valid_pports += 1
-    return valid_pports
+def day_4(puzzle, task):
 
-
-def day_4_1():
+    def read_passports():
+        raw_data = read_strings_from_file(puzzle)
+        passports = []
+        new_passport = []
+        for line in raw_data:
+            if line == "":
+                passports.append(" ".join(new_passport))
+                new_passport = []
+            else:
+                new_passport.append(line)
+        if new_passport:
+            passports.append(" ".join(new_passport))
+        return passports
 
     def is_valid(p):
         must_have_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
@@ -171,11 +149,6 @@ def day_4_1():
             if key not in p:
                 return False
         return True
-
-    print(day_4_logic(is_valid))
-
-
-def day_4_2():
 
     def between_nums(inf, sup, value):
         try:
@@ -211,7 +184,7 @@ def day_4_2():
             pattern = r"^\d{9}$"
             return bool(re.match(pattern, value))
 
-    def is_valid(p):
+    def is_valid_2(p):
         must_have_keys = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
         for key in must_have_keys:
             if key not in p:
@@ -220,10 +193,22 @@ def day_4_2():
                 return False
         return True
 
-    print(day_4_logic(is_valid))
+    passport_strings = read_passports()
+    passports = create_dict_from_string(passport_strings)
+    valid_pports = 0
+
+    if task == Task.FIRST:
+        validator_func = is_valid
+    else:
+        validator_func = is_valid_2
+
+    for p in passports:
+        if validator_func(p):
+            valid_pports += 1
+    return valid_pports
 
 
-def day_5_1():
+def day_5(puzzle, task):
 
     def code_to_id(code):
         code = code.replace("F", "0")
@@ -232,45 +217,35 @@ def day_5_1():
         code = code.replace("R", "1")
         return int(code, 2)
 
-    seat_codes = read_strings_from_file("day5-1")
+    seat_codes = read_strings_from_file(puzzle)
     seat_ids = list(map(code_to_id, seat_codes))
-    print(max(seat_ids))
 
-    return seat_ids
+    if task is Task.FIRST:
+        return max(seat_ids)
 
-
-def day_5_2():
-
-    seat_ids = day_5_1()
     seat_ids.sort()
-    print(seat_ids)
-
-    for i in range(len(seat_ids)-1):
-        if seat_ids[i+1] - seat_ids[i] > 1:
-            print("two seats: {} -- {} and the missing one is {}".format(seat_ids[i], seat_ids[i+1], seat_ids[i]+1))
+    for i in range(len(seat_ids) - 1):
+        if seat_ids[i + 1] - seat_ids[i] > 1:
+            return seat_ids[i]+1
 
 
 def main(args):
-    if args[0] == "day1-1":
-        day_1_1()
-    elif args[0] == "day1-2":
-        day_1_2()
-    elif args[0] == "day2-1":
-        day_2_1()
-    elif args[0] == "day2-2":
-        day_2_2()
-    elif args[0] == "day3-1":
-        day_3_1()
-    elif args[0] == "day3-2":
-        day_3_2()
-    elif args[0] == "day4-1":
-        day_4_1()
-    elif args[0] == "day4-2":
-        day_4_2()
-    elif args[0] == "day5-1":
-        day_5_1()
-    elif args[0] == "day5-2":
-        day_5_2()
+    task_map = {"first": Task.FIRST, "second": Task.SECOND}
+    task = task_map[args[1]]
+    day_map ={"day1": Puzzle.DAY_1, "day2": Puzzle.DAY_2, "day3": Puzzle.DAY_3, "day4": Puzzle.DAY_4,
+              "day5": Puzzle.DAY_5}
+    day = day_map[args[0]]
+
+    if day == Puzzle.DAY_1:
+        print(day_1(day, task))
+    elif day == Puzzle.DAY_2:
+        print(day_2(day, task))
+    elif day == Puzzle.DAY_3:
+        print(day_3(day, task))
+    elif day == Puzzle.DAY_4:
+        print(day_4(day, task))
+    elif day == Puzzle.DAY_5:
+        print(day_5(day, task))
     else:
         print("Unknown argument: {}, and the full list: {}".format(args[0], args))
 
